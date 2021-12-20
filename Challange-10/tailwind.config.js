c8b654127eace606567f4e54e4427f333034706b
Burak Demir <buraknasifdemir123@gmail.com>
2021-12-20 10:55:30 +0300

    Add Challange-10

---
+++ Challange-10/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    colors: {
+      "soft-blue": "hsl(215, 51%, 70%)",
+      "brand-cyan": "hsl(178, 100%, 50%)",
+      "main-bg": "hsl(217, 54%, 11%)",
+      "card-bg": "hsl(216, 50%, 16%)",
+      "brand-line": "hsl(215, 32%, 27%)",
+      white: "#fff",
+    },
+    extend: {
+      fontFamily: {
+        outfit: ['"Outfit", sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
